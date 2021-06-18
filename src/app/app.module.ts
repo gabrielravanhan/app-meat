caf738256f09fdd230f1e0f67701fda77af8f6f6
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-06-17 21:33:29 -0300

    Aula 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +28,9 @@ import { RestaurantsService } from './restaurants/restaurants.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [RestaurantsService],
   bootstrap: [AppComponent]
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,15 +2,25 @@ import { Restaurant } from './restaurant/restaurant.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { MEAT_API } from '../app.api';
+import { map } from 'rxjs/operators'
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private toastr: ToastrService) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get<Restaurant[]>(`http://localhost:3000/restaurants`);
+    return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`).pipe(
+      map(restaurants => restaurants)
+    );
+  }
+
+  exibirMensagens(titulo: string, mensagem: string, tipo: string): void {
+
   }
 }
