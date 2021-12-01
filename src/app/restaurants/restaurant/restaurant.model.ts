f622a7fc089fc049eca450cfdc7f91da224394e9
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-11-30 23:00:45 -0300

    Finalização

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -2,7 +2,9 @@ export interface Restaurant {
   id: string
   name: string
   category: string
+  about: string
   deliveryEstimate: string
   rating: number
   imagePath: string
+  hours: string
 }
